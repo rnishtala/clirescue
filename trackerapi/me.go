ec2bcaeba9fffad49d2715d2255e792156285b37
Raj Nishtala <raj.nishtala@gmail.com>
2022-08-02 22:46:36 -0400

    forking repo requires internal code changes

--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/rnishtala/clirescue/cmdutil"
+	"github.com/rnishtala/clirescue/user"
 )
 
 var (
